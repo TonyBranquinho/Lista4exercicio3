8bbd16130559824b85fbe8a476ab5828905d6743
Tony <antony.branquinho@gmail.com>
2024-01-09 16:10:30 -0300

    legal

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Digite o numero de casos.");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                double a = double.Parse(vet[0], CI);
+                double b = double.Parse(vet[1], CI);
+                double c = double.Parse(vet[2], CI);
+
+                double media = a * 0.2 + b * 0.3 + c * 0.5;
+                Console.WriteLine(media.ToString("F1", CI));
+            }
+        }
+    }
+}
